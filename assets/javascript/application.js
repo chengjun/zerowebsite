53da380e63b8cd294269937d1a4b8787c337d032
Wang Cheng-Jun <wangchj04@gmail.com>
2016-08-05 17:44:34 +0800

    img

--- assets/javascript/application.js
+++ assets/javascript/application.js
@@ -4,12 +4,12 @@ $(function(){
     if($(this).width() > 767){
 	// add the following lines
 	// http://stackoverflow.com/questions/14004318/show-random-image-from-array-in-javascript
-	    $imgArr = ["3.jpg", "4.jpg"];
-	    $basePath = "assets/images/";
-	    $rand = $imgArr[Math.floor(Math.random() * $imgArr.length)];
-	    $imag = $basePath + $rand;
-      $.backstretch($imag, {speed: 15}); 
-      //$.backstretch("assets/images/6.jpg", {speed: 150});
+	    //$imgArr = ["3.jpg", "4.jpg"];
+	    //$basePath = "assets/images/";
+	    //$rand = $imgArr[Math.floor(Math.random() * $imgArr.length)];
+	    //$imag = $basePath + $rand;
+      //$.backstretch($imag, {speed: 15});
+      $.backstretch("assets/images/3.jpg", {speed: 10});
       $('#backstretch').show();
     } else {
       $('#backstretch').hide();
