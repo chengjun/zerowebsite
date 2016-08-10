1e11a34ec89fe814a9f053e7680404a87db4f515
Wang Cheng-Jun <wangchj04@gmail.com>
2016-08-10 23:19:38 +0800

    cover

--- assets/javascript/application.js
+++ assets/javascript/application.js
@@ -9,7 +9,8 @@ $(function(){
 	    //$rand = $imgArr[Math.floor(Math.random() * $imgArr.length)];
 	    //$imag = $basePath + $rand;
       //$.backstretch($imag, {speed: 15});
-      $.backstretch("http://oaf2qt3yk.bkt.clouddn.com/3.jpg", {speed: 10});
+      //$.backstretch("http://oaf2qt3yk.bkt.clouddn.com/3.jpg", {speed: 10});
+      $.backstretch("http://oaf2qt3yk.bkt.clouddn.com/3_meitu_1.jpg", {speed: 10});
       $('#backstretch').show();
     } else {
       $('#backstretch').hide();
